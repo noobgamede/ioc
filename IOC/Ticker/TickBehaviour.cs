314148c08d27391f6e2597708653a41181a628cb
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 12:01:09 +0530

    TickBehaviour

---
+++ IOC/Ticker/TickBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+namespace IOC.Ticker
+{
+    public class TickBehaviour : MonoBehaviour
+    {
+        List<ITickable> _ticked = new List<ITickable>();
+        List<IPhysicallyTickable> _physicallyTicked = new List<IPhysicallyTickable>();
+        List<ILateTickable> _lateTicked = new List<ILateTickable>();
+
+        internal void Add(ITickable tickable)
+        {
+            _ticked.Add(tickable);
+        }
+
+        internal void Remove(ITickable tickable)
+        {
+            _ticked.Remove(tickable); 
+        }
+
+        internal void AddPhysical(IPhysicallyTickable tickable)
+        {
+            _physicallyTicked.Add(tickable);
+        }
+
+        internal void RemovePhysical(IPhysicallyTickable tickable)
+        {
+            _physicallyTicked.Remove(tickable);
+        }
+
+        internal void AddLate(ILateTickable tickable)
+        {
+            _lateTicked.Add(tickable);
+        }
+
+        internal void RemoveLate(ILateTickable tickable)
+        {
+            _lateTicked.Remove(tickable);
+        }
+    }
+
+}
+            
