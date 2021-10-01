c21eaa23ee3b164aa10377eba1d3c64c40c7ff48
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 09:32:03 +0530

    UnityRoot

---
+++ IOC/IoC/UnityRoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using IOC.IoC;
+
+public class UnityRoot : MonoBehaviour
+{
+}
+
+public class UnityRoot<T> : UnityRoot where T:class,ICompositionRoot,new() 
+{
+    T _applicationRoot;
+
+    private void Awake()
+    {
+        _applicationRoot = new T();
+        Init();
+    }
+
+    virtual protected void Init()
+    {
+        MonoBehaviour[] behaviours = this.transform.GetComponentsInChildren<MonoBehaviour>(true);
+        for(int i=0;i<behaviours.Length;++i)
+        {
+            _applicationRoot.container.Inject(behaviours[i]); 
+        }
+    }
+}
