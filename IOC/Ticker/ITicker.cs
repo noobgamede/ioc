6cf1deee4e0d581fbb5c8f82bfcda2d853de311a
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 11:45:28 +0530

    ITicker interface

---
+++ IOC/Ticker/ITicker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace IOC.Ticker
+{
+    public interface ITicker
+    {
+        void Add(ITickable tickable);
+        void Remove(ITickable tickable);
+
+        void AddPhysical(IPhysicallyTickable tickable);
+        void RemovePhysical(IPhysicallyTickable tickable);
+
+        void AddLate(ILateTickable tickable);
+        void RemoveLate(ILateTickable tickable);
+    }
+}
