df268f5226c24362d6a381a0b573fa22db3482c5
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 08:53:15 +0530

    GameObjectFactory

--- IOC/IoC/GameObjectFactory.cs
+++ IOC/IoC/GameObjectFactory.cs
@@ -24,12 +24,33 @@ namespace IOC.IoC
 
         public GameObject Build(string type)
         {
-            throw new NotImplementedException();
+            //DesignerByContract.Check.Require(prefabs.ContainsKey(type),"IGameObjectFactory - Invalid Prefab Type");
+            GameObject go = Build(prefabs[type][0]);
+
+            Vector3 scale = go.transform.localScale;
+            Quaternion rotation = go.transform.localRotation;
+            Vector3 position = go.transform.localPosition;
+
+            prefabs[type][1].transform.gameObject.SetActive(true);
+
+            go.transform.parent = prefabs[type][1].transform;
+
+            go.transform.localPosition = position;
+            go.transform.localRotation = rotation;
+            go.transform.localScale = scale;
+
+            return go;
         }
 
         public GameObject Build(GameObject go)
         {
-            throw new NotImplementedException();
+            GameObject copy = GameObject.Instantiate(go) as GameObject;
+            MonoBehaviour[] components = copy.GetComponentsInChildren<MonoBehaviour>(true);
+            for(int i=0;i<components.Length;++i)
+            {
+                _container.Inject(components[i]); 
+            }
+            return copy;
         }
     }
 }
