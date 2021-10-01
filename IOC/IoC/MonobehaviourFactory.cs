e6892920304cfdfe14e24de15ae322cb0cd321a1
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 07:34:34 +0530

    MonobehaviourFactory

---
+++ IOC/IoC/MonobehaviourFactory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+namespace IOC.IoC
+{
+    public class MonobehaviourFactory : IMonobehaviourFactory
+    {
+        IContainer _container;
+        public MonobehaviourFactory(IContainer container)
+        {
+            _container = container; 
+        }
+        public M Build<M>(Func<M> constructor) where M : Monobehaviour
+        {
+            M mb = (M)constructor();
+            _container.Inject(mb);
+            return mb;
+        }
+    }
+}
