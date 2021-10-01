1e301b8047a801e8906b51aaa489c14ad0a3cbf7
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 06:52:36 +0530

    IBinder

---
+++ IOC/IoC/IBinder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IOC.IoC
+{
+    public interface IBinder<Contractor> where Contractor:class
+    {
+        void AsSingle<T>(T instance) where T : class, Contractor;
+        void AsSingle<T>() where T : Contractor, new();
+        void ToFactory<T>(IProvider<T> provider) where T : class, Contractor;
+    }
+}
