09b1fdc021704ac4813d9c3e9797228e855fe219
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 07:02:21 +0530

    IContainer

---
+++ IOC/IoC/IContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace IOC.IoC
+{
+    public interface IContainer
+    {
+        IBinder<TContractor> Bind<TContractor>() where TContractor : class;
+        void BindSelf<TContractor>() where TContractor : class, new();
+        TContractor Build<TContractor>() where TContractor : class;
+        void Release<TContractor>() where TContractor : class;
+        TContractor Inject<TContractor>(TContractor instance);
+    }
+
+    public interface IInternalContainer
+    {
+        void Register<T, K>(System.Type type, K provider) where K : IProvider<T>; 
+    }
+}
