4af2d33e3926b6d63e085882e4626f0b4745c002
noob game dev <ghanshalamayank@gmail.com>
2021-10-01 09:19:29 +0530

    PrefabHolder

---
+++ IOC/IoC/PrefabHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using IOC.IoC;
+
+public class PrefabHolder : MonoBehaviour, IInitialize
+{
+    [Inject]internal IGameObjectFactory factory { set; private get; }
+    public GameObject prefab;
+    public bool createAtStartup;
+    public bool hideOnStartup;
+
+    public void OnDependencyInjected()
+    {
+        factory.AddPrefab(prefab,prefab.name,gameObject);
+        if(createAtStartup)
+        {
+            GameObject go = factory.Build(prefab.name);
+            transform.localPosition = Vector3.zero;
+            transform.localScale = Vector3.one;
+
+            if (hideOnStartup)
+                go.SetActive(false); 
+        }
+    }
+}
