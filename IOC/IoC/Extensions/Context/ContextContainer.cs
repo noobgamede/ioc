d56034503febad7cb98b92de2bc986ed960a43cc
noob game dev <ghanshalamayank@gmail.com>
2021-10-07 07:33:12 +0530

    Context Container

---
+++ IOC/IoC/Extensions/Context/ContextContainer.cs
@@ -0,0 +1,23 @@
+﻿using IOC.Context;
+
+namespace IOC.IoC
+{
+    public class ContextContainer:Container
+    {
+        IContextNotifier _contextNotifier;
+
+        public ContextContainer(IContextNotifier contextNotifier)
+        {
+            _contextNotifier = contextNotifier; 
+        }
+
+        protected override void OnInstanceGenerated<TContractor>(TContractor instance)
+        {
+            if (instance is IWaitForFrameInitialization)
+                _contextNotifier.AddFrameworkInitializationListener(instance as IWaitForFrameInitialization);
+
+            if (instance is IWaitForFrameDestruction)
+                _contextNotifier.AddFrameworkDestructionListener(instance as IWaitForFrameDestruction);
+        }
+    }
+}
